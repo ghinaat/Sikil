968513b948a3284f008fc309fee98b9116a0f699
ghinaat <ghina.athaya05@gmail.com>
2023-07-26 11:15:39 +0700

    updaate index-ghina

--- app/Http/Controllers/PengalamanKerjaController.php
+++ app/Http/Controllers/PengalamanKerjaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\PengalamanKerja;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PengalamanKerjaController extends Controller
