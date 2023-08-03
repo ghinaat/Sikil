62ab814762d796595d29bc077b268fdb1f958807
Kevin Almer <kevinalmer4@gmail.com>
2023-08-03 08:22:12 +0700

    hotfix migration users

--- database/migrations/2023_07_19_022028_create_users_table.php
+++ database/migrations/2023_07_19_022028_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id_users');
-            $table->string('kode_finger');
             $table->string('nama_pegawai',100);
             $table->string('email')->unique();
             $table->string('password');
