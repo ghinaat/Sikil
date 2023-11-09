a33b1a71332e09fd7a176df0d2776149b6e911c8
ghinaat <ghina.athaya05@gmail.com>
2023-11-09 13:28:00 +0700

    database peminjaman

--- database/migrations/2023_11_02_064819_create_peminjaman_barang_table.php
+++ database/migrations/2023_11_02_064819_create_peminjaman_barang_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->date('tgl_peminjaman');
             $table->date('tgl_pengembalian');
             $table->string('kegiatan');
-            $table->enum('status', ['diajukan' ,'dipinjam', 'dikembalikan', 'dikembalikan_sebagain']);
+            $table->enum('status', ['belum_diajukan' ,'diajukan' ,'dipinjam', 'dikembalikan', 'dikembalikan_sebagian']);
             $table->string('keterangan');
             $table->foreign('id_users')->references('id_users')->on('users')->onDelete('cascade');
             $table->enum('is_deleted', ['0', '1'])->default('0');
